7d17a8bf6b84fb0fac88500d7348651f20b725ed
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-06 21:18:01 +0545

    datatypes

---
+++ py02-Variables&Datatypes/type.py
@@ -0,0 +1,31 @@
+a = 30
+t = type(a) # <class 'int'>
+print(t)
+
+b = 3.14
+t = type(b) # <class 'float'>
+print(t)
+
+c = 3 + 4j
+t = type(c) # <class 'complex'>
+print(t)
+
+d = "Hello, World!"
+t = type(d) # <class 'str'>
+print(t)
+
+e = [1, 2, 3]
+t = type(e) # <class 'list'>
+print(t)
+
+f = (1, 2, 3)
+t = type(f) # <class 'tuple'>
+print(t)
+
+g = {1, 2, 3}
+t = type(g) # <class 'set'>
+print(t)
+
+h = {"name": "John", "age": 36}
+t = type(h) # <class 'dict'>
+print(t)
