41fbf1e34669528edf41dbfa1b92fa8acc9e393f
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-06 22:07:53 +0545

    string problems

---
+++ py03_string/02_problem.py
@@ -0,0 +1,5 @@
+letter = '''Dear <|Name|>, 
+You are selected!
+<|Date|>'''
+print(letter.replace("<|Name|>", "Tekendra").replace("<|Date|>", "12-12-2020"))
+# Write a Python program to generate a letter using the above letter template.
---
+++ py03_string/03_problem.py
@@ -0,0 +1,5 @@
+'''Write a Python program to 
+find the first occurrence of the 
+space in the given string ? '''
+name = "Tekendra is a good  boy and"
+print(name.find("  "))
