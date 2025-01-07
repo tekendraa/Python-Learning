ac0f035250199dcfdf214bb71b40cfc91c077269
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-07 15:23:03 +0545

    tuple problems

---
+++ py04_lists/problem1.py
@@ -0,0 +1,14 @@
+"""Create a list of 5 fruits. Accept fruit names from the user 
+ and add them to the list. Print the list."""
+fruits = []
+f1 = input("Enter fruit name: ")
+fruits.append(f1)
+f2 = input("Enter fruit name: ")
+fruits.append(f2)
+f3 = input("Enter fruit name: ")
+fruits.append(f3)
+f4 = input("Enter fruit name: ")
+fruits.append(f4)
+f5 = input("Enter fruit name: ")
+fruits.append(f5)
+print(fruits)
