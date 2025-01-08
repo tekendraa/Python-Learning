d3710a93c29bfb09f22ec070604f8bcedb111135
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-08 11:21:14 +0545

    es

---
+++ py05_dictionary&sets/problem06.py
@@ -0,0 +1,9 @@
+d = {}
+name = input("Enter friends name: ")
+lang = input("Enter friends language: ")
+d.update({name: lang})
+name = input("Enter friends name: ")
+lang = input("Enter friends language: ")
+d.update({name: lang})
+
+print(d)
