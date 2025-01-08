adf728297037bebc3470bce35c952b4ffea55f93
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-08 14:34:48 +0545

    loops

---
+++ py07_Loops/02_for_loops.py
@@ -0,0 +1,15 @@
+#For loops with lists
+l = [1, 4, 56, 34, 70]
+for i in l:
+    print(i)
+
+# For loops with Tuples
+t = (6, 231, 45, 133)
+for i in t:
+    print(i)
+
+# For loop with strings 
+s ="Tekendra"
+for i in s:
+    print(i)
+    
---
+++ py07_Loops/03_forwithelse.py
@@ -0,0 +1,8 @@
+l = [1,4,8,9]
+
+for item in l:
+    print(item)
+
+else:
+    print("done") # this is printed when the loops exhausts!
+    
