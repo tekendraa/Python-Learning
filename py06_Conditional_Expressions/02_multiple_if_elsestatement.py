139ec734369daafeeb66473f0c65ef0ecf7c582f
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-08 11:37:34 +0545

    multiple if else statement

---
+++ py06_Conditional_Expressions/02_multiple_if_elsestatement.py
@@ -0,0 +1,12 @@
+# Example of multiple if-else statements
+
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You are a child.")
+elif age >= 13 and age < 20:
+    print("You are a teenager.")
+elif age >= 20 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior.")
