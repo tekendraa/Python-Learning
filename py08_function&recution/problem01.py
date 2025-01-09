9d258b270d1aadb745f76ac2a8aa3657ec0ef607
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-09 09:10:56 +0545

    fun

---
+++ py08_function&recution/problem01.py
@@ -0,0 +1,14 @@
+a = 1
+b = 4
+c = 9
+
+def greatest(a, b, c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b 
+    elif(c>b and c>a):
+        return c
+    
+
+print(greatest(a, b, c))
