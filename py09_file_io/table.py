358e01bb23d615c33e9ceee68bd1dab90475075b
Tekendra Bhandari <tekendraa67@gmail.com>
2025-01-09 11:38:16 +0545

    tables

---
+++ py09_file_io/table.py
@@ -0,0 +1,15 @@
+import os
+
+def generateTable(n):
+    table = ""
+    for i in range(1, 11):
+        table += f"{n} X {i} = {n*i}\n"
+
+    # Ensure the directory exists
+    os.makedirs("tables", exist_ok=True)
+
+    with open(f"tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+for i in range(2, 21):
+    generateTable(i)
